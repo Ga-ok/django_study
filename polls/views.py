ba755adc8ed1b484035ce5c0d307c9636b7e247d
gaok <dlrkdhr321@gmail.com>
2021-08-03 17:52:37 +0900

    django view 사용, html 불러오기 및 연결

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,56 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
+from django.template import loader
+
+from django.http import Http404
+
+from django.shortcuts import get_object_or_404
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    # 1
+    # return HttpResponse("Hello, world.")
+
+    # 2
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+    # 3
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    # 4
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 # 3. 응답을 클라이언트에게 전달 
+
+
+# 클라이언트로부터 request를 받아서 response를 리턴 
+def detail(request, question_id):
+
+    # 1
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+
+    # 2
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
